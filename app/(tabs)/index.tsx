851250265e93600dea0d4d1cc69c06efc0823e0d
Vinisilvasn23 <vini456nascimento@gmail.com>
2024-05-30 09:07:47 -0300

    update btn color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -218,7 +218,7 @@ const styles = StyleSheet.create({
     color: 'red',
   },
   btn: {
-    backgroundColor: '#007BFF',
+    backgroundColor: '#f8b43a',
     padding: 12,
     borderRadius: 12,
     alignItems: 'center',
